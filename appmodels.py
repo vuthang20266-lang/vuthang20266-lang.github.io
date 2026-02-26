316571e1c6210e00edccc7a75e34ec501d164558
vuthang20266-lang <vuthang20266@gmail.com>
2026-02-27 00:15:56 +0700

    Update appmodels.py
    .

--- appmodels.py
+++ appmodels.py
@@ -1,9 +1,6 @@
-from app import db
-from flask_login import UserMixin
+from appinit import db
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
-    email = db.Column(db.String(200), unique=True)
-    password = db.Column(db.String(200))
-    role = db.Column(db.String(50), default="user")
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
