64487e1087ac31912c375e1f7782b94697e8a0f7
vuthang20266-lang <vuthang20266@gmail.com>
2026-02-27 00:07:50 +0700

    Update app.py
    .

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
+from flask import render_template
 
 @app.route("/")
 def home():
-    return "Web của tao đã hoạt động 🚀"
-
-if __name__ == "__main__":
-    app.run()
+    return render_template("index.html")
