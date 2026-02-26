91db3f9cc9daee5991ed17ba2dfb164981803d1e
vuthang20266-lang <vuthang20266@gmail.com>
2026-02-27 00:24:26 +0700

    Update appmodels.py
    .

--- appmodels.py
+++ appmodels.py
@@ -1,6 +1,8 @@
 from appinit import db
 
 class User(db.Model):
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
