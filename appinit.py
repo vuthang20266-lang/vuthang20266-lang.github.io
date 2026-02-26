c2552fb65916fef4db2ebe4c4935954eda339868
vuthang20266-lang <vuthang20266@gmail.com>
2026-02-26 23:06:11 +0700

    Create appinit.py
    .

---
+++ appinit.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from app.routes.auth import auth_bp
+    from app.routes.admin import admin_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(admin_bp)
+
+    return app
