78184b407cc42af01619cf2eb28c3886f91bb3e5
vuthang20266-lang <vuthang20266@gmail.com>
2026-02-27 00:24:57 +0700

    Update approutesadmin.py
    .

--- approutesadmin.py
+++ approutesadmin.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
-from flask_login import login_required
 
-admin_bp = Blueprint("admin", __name__)
+auth = Blueprint("auth", __name__)
 
-@admin_bp.route("/admin")
-@login_required
-def admin_dashboard():
-    return "Admin Dashboard"
+@auth.route("/")
+def home():
+    return "🔥 Trang chủ hoạt động"
+
+@auth.route("/login")
+def login():
+    return "🔐 Trang đăng nhập"
