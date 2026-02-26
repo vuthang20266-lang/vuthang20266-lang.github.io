43f3d5bee2f3126ba16471e76d9abe259cbdc5d8
vuthang20266-lang <vuthang20266@gmail.com>
2026-02-26 23:07:18 +0700

    Create appmodels.py
    .

---
+++ appmodels.py
@@ -0,0 +1,9 @@
+from app import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(200))
+    role = db.Column(db.String(50), default="user")
